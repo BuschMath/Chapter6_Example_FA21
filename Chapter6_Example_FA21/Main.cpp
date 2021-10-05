cac894ac8ef3ce0538f9c3e4df0a60f5e0d62e80
David Busch <buschdr@gmail.com>
2021-10-05 10:11:59 -0500

    End of File Loop

--- Chapter6_Example_FA21/Main.cpp
+++ Chapter6_Example_FA21/Main.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Calculate Fibonacci sequence to 5 digits.\n";
+	ifstream inFile;
+	inFile.open("C:/Users/busch/OneDrive for Business/Classes/C++/Presentations/Ch6Examples/InputEOFExample.dat");
 
-	bool test = true;
-	int n_1 = 1, n_2 = 1;
-	int n_3;
+	char data;
 
-	cout << n_1 << endl << n_2 << endl;
-
-	while (test)
+	if (!inFile)
 	{
-		n_3 = n_1 + n_2;
-		cout << n_3 << endl;
+		cout << "Can't find file!" << endl;
 
-		n_1 = n_2;
-		n_2 = n_3;
+		return 1;
+	}
 
-		if (n_2 >= 100000)
-			test = false;
+	inFile >> data; // Priming read
+
+	while (inFile)
+	{
+		cout << data << endl;
+		inFile >> data;
 	}
 
 	return 0;
